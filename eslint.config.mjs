453f6e339a72cddaf00af9adc41f18f4015ff8b2
David <d>
2025-08-19 11:10:25 -0500

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,9 @@ const eslintConfig = [
   {
     rules: {
       "@typescript-eslint/no-unused-vars": "warn", // Change from error to warning
+      "@typescript-eslint/no-explicit-any": "warn", // Warning instead of error
     }
+
   }
 ];
 
